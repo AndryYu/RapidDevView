b4b297d374dd61cf0950d276c3f4a6a5bdb50611
andryyu <andryyu5419@163.com>
2017-12-09 10:41:10 +0800

    inital code

---
+++ app/src/main/java/com/andryyu/view/custom/CustomActivity.java
@@ -0,0 +1,15 @@
+package com.andryyu.view.custom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.andryyu.view.R;
+
+public class CustomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom);
+    }
+}
